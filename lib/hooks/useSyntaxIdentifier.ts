777e0e4d5950e6b41a6a0cab4403edf9bb7f0e6e
Wonkers0 <wonk0542@gmail.com>
2023-07-27 11:48:28 +0300

    Reverting last commit :nerd_face:
    This reverts commit 4a6071aac07ebd630374c991b1e3bb0f2ad8240e.

--- lib/hooks/useSyntaxIdentifier.ts
+++ lib/hooks/useSyntaxIdentifier.ts
@@ -3,20 +3,5 @@ import "prismjs/components/prism-jsx.min";
 import { useMemo } from "react";
 
 export default function useSyntaxIdentifier(code: string, language: string) {
-  return useMemo(
-    () =>
-      deserializeCode(
-        Prism.highlight(serializeCode(code), Prism.languages[language], language)
-      ),
-    []
-  );
-}
-
-// Prism strips markup from the code which is extremely annoying
-function serializeCode(code: string): string {
-  return code.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-}
-
-function deserializeCode(code: string): string {
-  return code.replace(/&lt;/g, "<").replace(/&gt;g/, ">");
+  return useMemo(() => Prism.highlight(code, Prism.languages[language], language), []);
 }
