4a6071aac07ebd630374c991b1e3bb0f2ad8240e
Wonkers0 <wonk0542@gmail.com>
2023-07-27 10:38:05 +0300

    :ambulance: Markup from snippets is no longer stripped
    https://github.com/PrismJS/prism/issues/651

--- lib/hooks/useSyntaxIdentifier.ts
+++ lib/hooks/useSyntaxIdentifier.ts
@@ -3,5 +3,20 @@ import "prismjs/components/prism-jsx.min";
 import { useMemo } from "react";
 
 export default function useSyntaxIdentifier(code: string, language: string) {
-  return useMemo(() => Prism.highlight(code, Prism.languages[language], language), []);
+  return useMemo(
+    () =>
+      deserializeCode(
+        Prism.highlight(serializeCode(code), Prism.languages[language], language)
+      ),
+    []
+  );
+}
+
+// Prism strips markup from the code which is extremely annoying
+function serializeCode(code: string): string {
+  return code.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+}
+
+function deserializeCode(code: string): string {
+  return code.replace(/&lt;/g, "<").replace(/&gt;g/, ">");
 }
